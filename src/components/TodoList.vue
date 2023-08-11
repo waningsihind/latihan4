43f72507bccdf2cb39bc1b6e563a058b5205d649
waningsihind <checeelim@gmail.com>
2023-08-11 18:30:46 +0700

    third commit

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,39 +1,80 @@
 <template>
-    <div class="showHello">
-      <h1>{{ msg }}</h1>
-   
-    
-    <ol v-show="showTodos">
-    <td>
-      <li v-for="(todo, index) in todos" v-bind:key="index">
-        {{ todo }}
-    </li>
-    </td>
-    <td>
-        <button>Delete</button>
-    </td>
-    <td>
-        <button>Edit</button>
-    </td>
-    </ol>
-    </div>
-   
+  <div class="showHello">
+    <h1>{{ msg }}</h1>
+ 
+    <table v-show="showTodos">
+      <tbody>
+        <ol>
+          <li v-for="(todo, index) in todos" :key="index">
+            <tr>
+              <td v-if="editingIndex !== index">
+                {{ todo }}
+              </td>
+              <td v-else>
+                <input type="text" v-model="editedTodo" @keyup.enter="saveEdit(index)" />
+              </td>
+              <td>
+                <button @click="deleteTodo(index)">Delete</button>
+              </td>
+              <td>
+                <button @click="toggleEdit(index)">
+                  {{ editingIndex === index ? 'Save' : 'Edit' }}
+                </button>
+              </td>
+            </tr>
+          </li>
+        </ol>
+      </tbody>
+    </table>
+  </div>
 </template>
+
 <script>
 export default {
-    name: 'TodoList',
-    computed: {
+  name: 'TodoList',
+  data() {
+    return {
+      editingIndex: -1,
+      editedTodo: '',
+    };
+  },
+  computed: {
     showTodos: function () {
       return this.todos.length > 0;
     },
   },
-    props: {
-        msg: String,
-        todos: Array
+  props: {
+    msg: String,
+    todos: Array,
+  },
+  methods: {
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
     },
+    toggleEdit(index) {
+      if (this.editingIndex === index) {
+        this.saveEdit(index);
+      } else {
+        this.startEdit(index);
+      }
+    },
+    startEdit(index) {
+      this.editingIndex = index;
+      this.editedTodo = this.todos[index];
+    },
+    cancelEdit() {
+      this.editingIndex = -1;
+      this.editedTodo = '';
+    },
+    saveEdit(index) {
+      this.todos[index] = this.editedTodo;
+      this.editingIndex = -1;
+      this.editedTodo = '';
+    },
+  },
 };
 </script>
 
 <style scoped>
-
+/* Add your scoped styles here */
 </style>
