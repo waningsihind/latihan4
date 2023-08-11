f3a40338f7cba48aa93c29f60040697770acebc3
waningsihind <checeelim@gmail.com>
2023-08-11 15:45:59 +0700

    second commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="showHello">
+      <h1>{{ msg }}</h1>
+   
+    
+    <ol v-show="showTodos">
+    <td>
+      <li v-for="(todo, index) in todos" v-bind:key="index">
+        {{ todo }}
+    </li>
+    </td>
+    <td>
+        <button>Delete</button>
+    </td>
+    <td>
+        <button>Edit</button>
+    </td>
+    </ol>
+    </div>
+   
+</template>
+<script>
+export default {
+    name: 'TodoList',
+    computed: {
+    showTodos: function () {
+      return this.todos.length > 0;
+    },
+  },
+    props: {
+        msg: String,
+        todos: Array
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="showHello">
+    <TodoList msg="Todo List" :todos="todos"/>
+
+  <form @submit.prevent="onSubmit()">
+  <td>
+    <input v-model="name" type= "text" class= "inputTodos" placeholder="Input your Todo List here..." /> 
+  </td>
+  <td>
+    <button class="button" type="submit">Tambahkan</button>
+  </td>
+  <p>{{ showHebat }}</p>
+</form>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList.vue'
+
+export default {
+  name: "TodoView",
+  components: {
+    TodoList,
+  },
+  data (){
+    return {
+      name : '',
+      showError: false,
+      todos: [],
+    };
+  },
+  computed: {
+    showHebat: function () {
+      if (this.todos.length >= 4) {
+        return "Hebat!";
+      }
+      return "";
+    },
+  },
+  methods : {
+    onSubmit: function () {
+    if (!this.name) {
+        this.showError = true;
+        return false,
+        alert("Masukkan Todo!");
+      }
+      this.todos.push(this.name);
+      this.name = "";
+      this.showError = false;
+    },
+  },
+}
+</script>
+<style>
+.h1 {
+    padding-top: 20px;
+    font-size: large;
+    font-weight: bold;
+}
+.name {
+    width: 100%S;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+    font-size: 20px;
+}
+.button{
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+}
+</style>
